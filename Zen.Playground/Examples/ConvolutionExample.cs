4e02a4b0206ec6090b5c0faa1fda1274aef0011d
Andrii Horbokon <andrii.horbokon@gmail.com>
2024-10-24 18:22:08 +0300

    Convolution example

--- Zen.Playground/Examples/ConvolutionExample.cs
+++ Zen.Playground/Examples/ConvolutionExample.cs
@@ -1,98 +1,103 @@
 ﻿using Zen.CUDA;
+using Zen.CUDA.Interop;
+using Zen.CUDA.Wrappers;
 
 namespace Zen.Playground.Examples;
 
-public static class ConvolutionExample
+public static unsafe class ConvolutionExample
 {
-    public static void Conv1D()
+    public static void Run()
     {
-        using var array = HostArray.Allocate<float>(16);
-
-        for (var i = 0; i < array.Size; ++i)
+        nuint size;
+        zenConv2dPlan* plan;
+        zenConv2dParams parameters = new zenConv2dParams
         {
-            array[i] = i + 1;
+            input_n = 1,
+            input_h = 8,
+            input_w = 8,
+            input_c = 1,
+            filter_c = 1,
+            filter_h = 3,
+            filter_w = 3,
+            padding_h = 1,
+            padding_w = 1,
+            stride_h = 1,
+            stride_w = 1,
+            dilation_h = 1,
+            dilation_w = 1,
+            output_h = 8,
+            output_w = 8,
+        };
+
+        Shape input_shape = [
+            parameters.input_n,
+            parameters.input_h,
+            parameters.input_w,
+            parameters.input_c];
+        
+        Shape filter_shape = [
+            parameters.filter_c,
+            parameters.filter_h,
+            parameters.filter_w,
+            parameters.input_c];
+        
+        Shape output_shape = [
+            parameters.input_n,
+            parameters.output_h,
+            parameters.output_w,
+            parameters.filter_c];
+
+        using var host_input  = HostTensor.Allocate<float>(input_shape);
+        using var host_filter = HostTensor.Allocate<float>(filter_shape);
+        using var host_output = HostTensor.Allocate<float>(output_shape);
+        
+        using var input  = DeviceTensor.Allocate<float>(input_shape);
+        using var filter = DeviceTensor.Allocate<float>(filter_shape);
+        using var output = DeviceTensor.Allocate<float>(output_shape);
+        
+        for (var i = 0; i < host_input.Array.Size; ++i)
+        {
+            host_input.Array[i] = 1;
+        }
+
+        for (var i = 0; i < host_filter.Array.Size; ++i)
+        {
+            host_filter.Array[i] = 1f / filter_shape[1..].Size; // avg
+            host_filter.Array[i] = 1f; // sum
         }
         
-        var original = Tensor.Create(shape: [16], array);
-
-        var kernel = Shape.Create([3], original.Shape.Strides); // original strides = no dilation
-        var shape  = Shape.Create(
-            extents: [..original.Shape.Extents, ..kernel.Extents],
-            strides: [..original.Shape.Strides, ..kernel.Strides]);
-
-        var conv = Tensor.Create(shape, array);
-        
-        Utils.WriteLine(original);
-        
-        // original(16) =
-        // [  1.00   2.00   3.00   4.00   5.00   6.00   7.00   8.00   9.00  10.00  11.00  12.00  13.00  14.00  15.00  16.00 ]
-        
-        Utils.WriteLine(conv);
-
-        // conv(16,3) =
-        // [[  1.00   2.00   3.00 ]
-        //  [  2.00   3.00   4.00 ]
-        //  [  3.00   4.00   5.00 ]
-        //  [  4.00   5.00   6.00 ]
-        //  [  5.00   6.00   7.00 ]
-        //  [  6.00   7.00   8.00 ]
-        //  [  7.00   8.00   9.00 ]
-        //  [  8.00   9.00  10.00 ]
-        //  [  9.00  10.00  11.00 ]
-        //  [ 10.00  11.00  12.00 ]
-        //  [ 11.00  12.00  13.00 ]
-        //  [ 12.00  13.00  14.00 ]
-        //  [ 13.00  14.00  15.00 ]
-        //  [ 14.00  15.00  16.00 ]
-        //  [ 15.00  16.00   0.00 ]
-        //  [ 16.00   0.00   0.00 ]]
-    }
-
-    public static void Conv2D()
-    {
-        using var array = HostArray.Allocate<float>(16);
-        
-        for (var i = 0; i < array.Size; ++i)
+        for (var i = 0; i < host_output.Array.Size; ++i)
         {
-            array[i] = i + 1;
+            host_output.Array[i] = float.NaN;
         }
 
-        var original = Tensor.Create(shape: [4, 4], array);
+        using var stream = new CudaStream();
+
+        host_input.CopyTo(input, stream);
+        host_filter.CopyTo(filter, stream);
+        stream.Synchronize();
+
+        LibZen.zenCreateConv2dPlan(&plan, &parameters);
+        LibZen.zenGetConv2dWorkspaceSize(plan, &size);
+
+        var ws = DeviceArray.Allocate<byte>((int)size);
         
-        var kernel = Shape.Create([2, 2], original.Shape.Strides); // original strides = no dilation
-        var shape  = Shape.Create(
-            extents: [..original.Shape.Extents, ..kernel.Extents],
-            strides: [..original.Shape.Strides, ..kernel.Strides]);
+        LibZen.zenExecuteConv2d(
+            plan,
+            input.Array.Pointer,
+            filter.Array.Pointer,
+            output.Array.Pointer,
+            1.0f,
+            0.0f,
+            output.Array.Pointer,
+            ws.Pointer,
+            stream.Pointer);
+
+        stream.Synchronize();
+        output.CopyTo(host_output);
         
-        var conv = Tensor.Create(shape, array);
-        
-        Utils.WriteLine(original);
-        
-        // original(4,4) =
-        // [[  1.00   2.00   3.00   4.00 ]
-        //  [  5.00   6.00   7.00   8.00 ]
-        //  [  9.00  10.00  11.00  12.00 ]
-        //  [ 13.00  14.00  15.00  16.00 ]]
-        
-        Utils.WriteLine(conv);
-        
-        // conv(4,4,2,2) =
-        // [[[[  1.00   2.00 ]
-        //    [  5.00   6.00 ]]
-        //   [[  2.00   3.00 ]
-        //    [  6.00   7.00 ]]
-        //   [[  3.00   4.00 ]
-        //    [  7.00   8.00 ]]
-        //   [[  4.00   5.00 ]
-        //    [  8.00   9.00 ]]]
-        //  [[[  5.00   6.00 ]
-        //    [  9.00  10.00 ]]
-        //   [[  6.00   7.00 ]
-        //    [ 10.00  11.00 ]]
-        //   [[  7.00   8.00 ]
-        //    [ 11.00  12.00 ]]
-        //   [[  8.00   9.00 ]
-        //    [ 12.00  13.00 ]]]
-        // ....
+        Utils.WriteLine(host_input.Slice(0, .., .., 0));
+        Utils.WriteLine(host_filter.Slice(0, .., .., 0));
+        Utils.WriteLine(host_output.Slice(0, .., .., 0));
     }
 }
--- Zen.Playground/Program.cs
+++ Zen.Playground/Program.cs
@@ -1,10 +1,8 @@
-﻿using Zen.CUDA;
-using Zen.CUDA.Interop;
-using Zen.CUDA.Wrappers;
+﻿using Zen.Playground.Examples;
 
 namespace Zen.Playground;
 
-internal static unsafe class Program
+internal static class Program
 {
     public static void Main()
     {
@@ -12,104 +10,7 @@ internal static unsafe class Program
         // GraphExample.Run();
         // EventExample.Run();
         // SliceExample.Run();
-        // ConvolutionExample.Conv1D();
-        // ConvolutionExample.Conv2D();
 
-        Run();
-    }
-
-    private static void Run()
-    {
-        nuint size;
-        zenConv2dPlan* plan;
-        zenConv2dParams parameters = new zenConv2dParams
-        {
-            input_n = 1,
-            input_h = 8,
-            input_w = 8,
-            input_c = 1,
-            filter_c = 1,
-            filter_h = 3,
-            filter_w = 3,
-            padding_h = 1,
-            padding_w = 1,
-            stride_h = 1,
-            stride_w = 1,
-            dilation_h = 1,
-            dilation_w = 1,
-            output_h = 8,
-            output_w = 8,
-        };
-
-        Shape input_shape = [
-            parameters.input_n,
-            parameters.input_h,
-            parameters.input_w,
-            parameters.input_c];
-        
-        Shape filter_shape = [
-            parameters.filter_c,
-            parameters.filter_h,
-            parameters.filter_w,
-            parameters.input_c];
-        
-        Shape output_shape = [
-            parameters.input_n,
-            parameters.output_h,
-            parameters.output_w,
-            parameters.filter_c];
-
-        using var host_input  = HostTensor.Allocate<float>(input_shape);
-        using var host_filter = HostTensor.Allocate<float>(filter_shape);
-        using var host_output = HostTensor.Allocate<float>(output_shape);
-        
-        using var input  = DeviceTensor.Allocate<float>(input_shape);
-        using var filter = DeviceTensor.Allocate<float>(filter_shape);
-        using var output = DeviceTensor.Allocate<float>(output_shape);
-        
-        for (var i = 0; i < host_input.Array.Size; ++i)
-        {
-            host_input.Array[i] = 1;
-        }
-
-        for (var i = 0; i < host_filter.Array.Size; ++i)
-        {
-            host_filter.Array[i] = 1f / filter_shape[1..].Size; // avg
-            host_filter.Array[i] = 1f; // sum
-        }
-        
-        for (var i = 0; i < host_output.Array.Size; ++i)
-        {
-            host_output.Array[i] = float.NaN;
-        }
-
-        using var stream = new CudaStream();
-
-        host_input.CopyTo(input, stream);
-        host_filter.CopyTo(filter, stream);
-        stream.Synchronize();
-
-        LibZen.zenCreateConv2dPlan(&plan, &parameters);
-        LibZen.zenGetConv2dWorkspaceSize(plan, &size);
-
-        var ws = DeviceArray.Allocate<byte>((int)size);
-        
-        LibZen.zenExecuteConv2d(
-            plan,
-            input.Array.Pointer,
-            filter.Array.Pointer,
-            output.Array.Pointer,
-            1.0f,
-            0.0f,
-            output.Array.Pointer,
-            ws.Pointer,
-            stream.Pointer);
-
-        stream.Synchronize();
-        output.CopyTo(host_output);
-        
-        Utils.WriteLine(host_input.Slice(0, .., .., 0));
-        Utils.WriteLine(host_filter.Slice(0, .., .., 0));
-        Utils.WriteLine(host_output.Slice(0, .., .., 0));
+        ConvolutionExample.Run();
     }
 }
